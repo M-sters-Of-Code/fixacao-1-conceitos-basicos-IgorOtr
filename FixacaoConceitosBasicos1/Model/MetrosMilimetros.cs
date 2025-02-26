fd2074111804532fb4779b14f08bc4c9db36f667
IgorOtr <igorotero.muniz@gmail.com>
2025-02-26 19:38:38 -0300

    TESTE DE PUSH

---
+++ FixacaoConceitosBasicos1/Model/MetrosMilimetros.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixacaoConceitosBasicos1.Model
+{
+    public class MetrosMilimetros
+    { 
+        public static int Converter(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+    }
+}
